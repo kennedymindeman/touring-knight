4939d2391b9c3559f15e28e4588515e38a9e7177
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-06-12 20:22:56 -0500

    Make board and check that the rows and columns are the right size

---
+++ src/touring_knight.py
@@ -0,0 +1,3 @@
+class Board:
+    def __init__(self, num_rows: int, num_cols: int) -> None:
+        raise NotImplemented
---
+++ test/test_touring_knight.py
@@ -0,0 +1,7 @@
+from src.touring_knight import Board
+
+
+def test_place_knight() -> None:
+    board = Board(num_rows=5, num_cols=5)
+    assert board.num_rows=5
+    assert board.num_cols=5
