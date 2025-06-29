9d1f1cf103099f9cd4295eba83119856ad95ab2b
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-06-29 15:18:03 -0500

    Add find_best_move method

--- src/touring_knight.py
+++ src/touring_knight.py
@@ -45,11 +45,8 @@ class Board:
         self.knight_position = (row, col)
         self.visited.add((row, col))
 
-    def get_possible_knight_moves(self) -> list[tuple[int, int]]:
-        if self.knight_position is None:
-            raise NoKnightOnBoardException("There must be a knight on the board to move it")
+    def get_possible_knight_moves(self, row: int, col: int) -> list[tuple[int, int]]:
         valid_next_positions = []
-        row, col = self.knight_position
         for row_offset, col_offset in Board.KNIGHT_OFFSETS:
             valid_next_position = (row + row_offset, col + col_offset)
             if self.is_valid_position(*valid_next_position):
@@ -57,7 +54,21 @@ class Board:
         return valid_next_positions
 
     def move_knight(self, row: int, col: int) -> None:
-        if (row, col) not in self.get_possible_knight_moves():
+        if self.knight_position is None:
+            raise NoKnightOnBoardException("There must be a knight on the board to move it")
+        if (row, col) not in self.get_possible_knight_moves(*self.knight_position):
             raise InvalidKnightMove(f"Knight cannot move from {self.knight_position} to {(row, col)}")
         self.knight_position = (row, col)
         self.visited.add((row, col))
+
+    def find_best_next_move(self) -> tuple[int, int] | None:
+        if self.knight_position is None:
+            raise NoKnightOnBoardException("There must be a knight on the board to move it")
+        least_cost = None
+        best_move = None
+        for move in self.get_possible_knight_moves(*self.knight_position):
+            cost = len(self.get_possible_knight_moves(*move))
+            if not least_cost or cost < least_cost:
+                least_cost = cost
+                best_move = move
+        return best_move
