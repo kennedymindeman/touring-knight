44840e5e360c86965647b7a91701d979fee4c933
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-06-29 22:21:08 -0500

    Add timer to main function

--- __main__.py
+++ __main__.py
@@ -1,10 +1,17 @@
+import timeit
 from src.touring_knight import Board
 
 
 def main():
-    board = Board(rows=10, cols=10)
+    number_of_times = 100
+    print(f"{timeit.timeit(solve, number=number_of_times) / number_of_times:.2e}[seconds]")
+    print(solve().move_stack)
+
+
+def solve():
+    board = Board(rows=8, cols=8)
     board.solve()
-    print(board.move_stack)
+    return board
 
 
 if __name__ == "__main__":
